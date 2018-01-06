d38ac36e049dfac7254251c5a264e3b6173a6592
Sheik Farhan <shalfarh@microsoft.com>
2018-01-07 00:08:21 +0530

    Added Logging.

--- Artifacts/linux-ngrok/linux-ngrok.sh
+++ Artifacts/linux-ngrok/linux-ngrok.sh
@@ -1,4 +1,28 @@
-# Command Line Arguments
+# Script to install and setup ngrok Azure DevTest lab Linux VMs.
+#
+# NOTE: Intended for use by the Azure DevTest Lab artifact system.
+#
+# Usage: linux-ngrok.sh <YOUR_AUTHKEY_HERE>
+
+USAGE_STRING="Usage: linux-ngrok.sh <YOUR_AUTHKEY_HERE>"
+
+LOGCMD='logger -i -t AZDEVTST_APTPKG --'
+which logger
+if [ $? -ne 0 ] ; then
+    LOGCMD='echo [AZDEVTST_APTPKG] '
+fi
+
+$LOGCMD "Setting up ngrok.. Command line given:"
+# Cannot use logger here, as the -- are interpreted by that command
+$LOGCMD "   $@"
+
+# Check for minimum number of parameters first - must be at minimum 1
+if [ $# -lt 1 ] ; then
+  $LOGCMD "ERROR: This script needs auth token as an argument."
+  $LOGCMD "$USAGE_STRING"
+  exit 1
+fi  
+
 ## Authentication Token ##
 AUTH_TOKEN=$1
 
@@ -20,6 +44,32 @@ else
     # 32 Bit
     cp ./ngrok-stable-linux-386/ngrok /opt/ngrok/ngrok
 fi
+
+# Check if binary copy was success
+if [ $? -eq 0 ]; then
+    $LOGCMD "Successfully copied ngrok binary."
+    echo "Successfully copied ngrok binary."
+else
+    $LOGCMD "Here's some debug information.."
+    echo "Here's some debug information.."
+
+    $LOGCMD "Current working directory"
+    echo "Current working directory"
+
+    $LOGCMD `pwd`
+    echo `pwd`
+
+    $LOGCMD "Files in current working directory.."
+    echo "Files in current working directory.."
+
+    RESULT=$(ls `pwd`)
+    $LOGCMD $RESULT
+    echo $RESULT
+    ls -al `pwd`
+    
+    exit 1
+fi
+
 
 chmod +x /opt/ngrok/ngrok
 
