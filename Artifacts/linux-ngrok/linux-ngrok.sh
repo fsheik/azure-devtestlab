24d81d2a8f29076e1857d125211e28de07bb96c5
Sheik Farhan <shalfarh@microsoft.com>
2018-01-06 18:27:52 +0530

    Added ngrok artifact

---
+++ Artifacts/linux-ngrok/linux-ngrok.sh
@@ -0,0 +1,29 @@
+# Command Line Arguments
+## Authentication Token ##
+AUTH_TOKEN=$1
+
+ARCHITECTURE=`uname -m`
+
+## ngrok binary ##
+mkdir -p /opt/ngrok
+
+if [ $ARCHITECTURE == 'x86_64' ];
+then
+    # 64 Bit
+    cp ./ngrok-stable-linux-amd64/ngrok /opt/ngrok/ngrok
+else
+    # 32 Bit
+    cp ./ngrok-stable-linux-386/ngrok /opt/ngrok/ngrok
+fi
+
+chmod +x /opt/ngrok/ngrok
+
+## ngrok config file ##
+sed -i "s/__AUTH_TOKEN_HERE__/$AUTH_TOKEN/g" ngrok.yml
+
+cp ./ngrok.yml /opt/ngrok/ngrok.conf
+
+## Setup ngrok as a service ##
+cp ./ngrok.upstart.conf /etc/init/ngrok.conf
+
+service ngrok
